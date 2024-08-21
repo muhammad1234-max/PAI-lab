5664bc348eeef8ae46bf1f96a8a21ca7a87b4e7a
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2024-08-21 09:52:20 +0500

    Create q2.py

---
+++ LAB1/q2.py
@@ -0,0 +1,17 @@
+num1= int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+operation = str(input("enter the operation you want to conduct(+,-,/,*): "))
+
+
+if operation == '+':
+    print( num1 + num2)
+elif operation == '-':
+    print( num1 - num2)
+elif operation == '/':
+    if num2==0:
+        raise("zero division error")
+    else:
+        print( num1/num2)
+else:
+    print( num1*num2)
+    
